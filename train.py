cd77334eeb8554e30e9b68f46ecdff0dcd9c2dfc
benestump <6yeargeun@gmail.com>
2021-03-25 22:45:10 +0900

    Finished train file for multi-GPU

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from models.MobileNetV2 import MobileNetV2 
 import math
+import os
 
 import tensorflow_datasets as tfds
 import urllib3
@@ -27,81 +28,20 @@ def normalize_and_resize_img(image, label):
 
     return tf.cast(image, tf.float32)/255., label
 
-def apply_normalize_on_dataset(ds, is_test=False, batch_size=16):
-    ds = ds.map(normalize_and_resize_img, num_parallel_calls=1)
-    ds = ds.batch(batch_size)
-
-    if not is_test:
-        ds=ds.repeat()
-        ds=ds.shuffle(200)
-
-    ds = ds.prefetch(tf.data.experimental.AUTOTUNE)
-
-    return ds
-
-class Trainer(object):
-    def __init__(self,
-                 model,
-                 epochs,
-                 global_batch_size,
-                 strategy,
-                 initial_learning_rate,
-                 start_epoch=1,
-                 tensorboard_dir='./logs'):
-        
-        self.start_epoch = start_epoch
-        self.model = model
-        self.epochs = epochs
-        self.strategy = strategy 
-        self.global_batch_size = global_batch_size
-        self.loss_object = tf.keras.losses.CategoricalCrossentropy()
-        self.optimizer = tf.keras.optimizers.SGD(learning_rate=initial_learning_rate)
-        self.model = model
-        self.current_learning_rate = initial_learning_rate
-        self.last_val_loss = math.inf
-        self.lowest_val_loss = math.inf
-        self.patience_count = 0
-        self.max_patience = 10
-        self.tensorboard_dir = tensorboard_dir
-        self.best_model = None
-        self.version = version 
-
-    def lr_decay(self):
-        if self.patience_count >= self.max_patience:
-            self.current_learning_rate /= 10.0
-            self.patience_count = 0
-        elif self.last_val_loss == self.lowest_val_loss:
-            self.patience_count = 0
-        self.patience_count += 1
-
-        self.optimizer.learning_rate = self.current_learning_rate
-
-    def lr_decay_step(self, epoch):
-        if epoch == 25 or epoch == 50 or epoch == 75:
-            self.current_learning_rate /= 10.0
-        self.optimizer.learning_rate = self.current_learning_rate
-
-    def run(self, train_dist_dataset, val_dist_dataset):
-
-
-    def save_model(self, epoch, loss):
-        model_name = f'./models/MobileNetV2-epoch-{epoch}-loss-{loss:.4f}.h5'
-        self.model.save_weights(model_name)
-        self.best_model = model_name
-        print(f'Model {model_name} saved.')
-
 def create_dataset(tfrecords, batch_size, is_train):
 
     dataset = tfrecords.map(normalize_and_resize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-
+    
     if is_train:
-        dataset = dataset.shuffle(batch_size)
+        dataset = dataset.shuffle(200)
     
     dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
     
+    return dataset
 
 
-def train(dataset='cifar10',epochs, start_epoch, leargning_rate, tensorboard_dir, checkpoint, batch_size, train_tfrecords, val_tfrecords):
+def train(dataset,input_shape, epochs, leargning_rate, batch_size):
 
     strategy = tf.distribute.MirroredStrategy()
     global_batch_size = strategy.num_replicas_in_sync * batch_size
@@ -113,31 +53,63 @@ def train(dataset='cifar10',epochs, start_epoch, leargning_rate, tensorboard_dir
     train_dataset = create_dataset(ds_train, global_batch_size, is_train='True')
     val_dataset = create_dataset(ds_test, global_batch_size, is_train='False')
 
-automatic_gpu_usage()
+    if not os.path.exists(os.path.join('./models_save')):
+        os.makedirs(os.path.join('./models_save/'))
+
+    with strategy.scope():
+
+        model = MobileNetV2(input_shape=input_shape, classes=10)
+        
+        model.compile(
+            loss='sparse_categorical_crossentropy',
+            optimizer=tf.keras.optimizers.SGD(momentum=0.9),
+            metrics=['accuracy'],
+        )
+
+    checkpoint_dir = './training_checkpoints'
+    if not os.path.exists(os.path.join(checkpoint_dir)):
+        os.makedirs(os.path.join(checkpoint_dir))
+
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
+    def decay(epoch):
+        if epoch < 10:
+            return leargning_rate
+        elif epoch >= 10 and epoch < 20:
+            return leargning_rate/10.
+        else:
+            return leargning_rate/10.
+    class PrintLR(tf.keras.callbacks.Callback):
+        def on_epoch_end(self, epoch, logs=None):
+            print('\n에포크 {}의 학습률은 {}입니다.'.format(epoch + 1,
+                                                      model.optimizer.lr.numpy()))  
+        
+    callbacks = [
+        tf.keras.callbacks.TensorBoard(log_dir='./logs'),
+        tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix,
+                                         save_weights_only=True),
+        tf.keras.callbacks.LearningRateScheduler(decay),
+        PrintLR()
+    ]
 
 
-
-
-ds_train = apply_normalize_on_dataset(ds_train, batch_size=BATCH_SIZE)
-ds_test = apply_normalize_on_dataset(ds_test, batch_size=BATCH_SIZE)
-
-model = MobileNetV2(input_shape=INPUT_SHAPE, classes=10)
-
-# print(model.summary())
-
-model.compile(
-    loss='sparse_categorical_crossentropy',
-    optimizer=tf.keras.optimizers.SGD(lr=0.01, momentum=0.9),
-    metrics=['accuracy'],
-)
-
-history_MBNetV2 = model.fit(
-    ds_train, 
-    steps_per_epoch=int(40000/BATCH_SIZE),
-    validation_steps=int(10000/BATCH_SIZE),
-    epochs=EPOCH,
-    validation_data=ds_test,
+    history_MBNetV2 = model.fit(
+    train_dataset, 
+    epochs=epochs,
+    validation_data=val_dataset,
     verbose=1,
     use_multiprocessing=True,
-)
- 
+    callbacks=callbacks
+    )
+    
+
+if __name__ == "__main__":
+    dataset = "cifar10"
+    epochs = 100
+    batch_size = 16
+    learning_rate = 0.01
+    INPUT_SAHPE=(32, 32, 3)
+
+    automatic_gpu_usage()
+
+    train(dataset=dataset,input_shape=INPUT_SAHPE, epochs=epochs, leargning_rate=learning_rate,batch_size=batch_size)
